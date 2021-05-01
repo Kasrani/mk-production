0404dd3b78873afd8fd69462f215d56e766d4b01
Kasrani <kasrani.mourad@gmail.com>
2021-05-01 02:56:58 +0200

    fix build heroku app :white_check_mark:

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const app = express()
 // serves files our dist directory which now contains out indexedDB.html file
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
+// this * route is to serve project on different page routes except
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
 const port = process.env.PORT || 8080
 app.listen(port)
 
