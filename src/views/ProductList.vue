b9e7d784495d70e905aa59497a7a0a0206cf1f80
Kasrani <kasrani.mourad@gmail.com>
2021-05-07 01:34:27 +0200

    clean & build commit

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -1,212 +1,258 @@
 <template>
-  <h1>listeeeeeeeeeeeeeeeeeeeeeeeeeee</h1>
-</template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
-<!--
-<template>
-    <v-data-table
-    :headers="headers"
-    :items="products"
-    sort-by="calories"
-    class="elevation-1"
+<v-row class="pl-4 pr-4">
+  <v-col>
+  <v-card
+    :loading="loading"
+    class="mx-auto my-6"
+    max-width="374"
   >
-  <template v-slot:top>
-      <v-toolbar
-        flat
-      >
-        <v-toolbar-title>Mes Produits</v-toolbar-title>
-        <v-divider
-          class="mx-4"
-          inset
-          vertical
-        ></v-divider>
-        <v-spacer></v-spacer>
-        <v-dialog
-          v-model="dialog"
-          max-width="500px"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              color="primary"
-              dark
-              class="mb-2"
-              v-bind="attrs"
-              v-on="on"
-            >
-              New Item
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="headline">{{ formTitle }}</span>
-            </v-card-title>
-
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.name"
-                      label="Nom"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.price"
-                      label="Prix"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                color="blue darken-1"
-                text
-                @click="close"
-              >
-                Cancel
-              </v-btn>
-              <v-btn
-                color="blue darken-1"
-                text
-                @click="save"
-              >
-                Save
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="500px">
-          <v-card>
-            <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-toolbar>
+    <template slot="progress">
+      <v-progress-linear
+        color="deep-purple"
+        height="10"
+        indeterminate
+      ></v-progress-linear>
     </template>
-    <template v-slot:item.actions="{ item }">
-      <v-icon
-        small
-        class="mr-2"
-        @click="editItem(item)"
+
+    <v-img
+      height="250"
+      src="https://i.imgur.com/1NNGyBy.png"
+    ></v-img>
+
+    <v-card-title>SÉLECTION BON PLAN</v-card-title>
+
+    <v-card-text>
+      <v-row
+        align="center"
+        class="mx-0"
       >
-        mdi-pencil
-      </v-icon>
-      <v-icon
-        small
-        @click="deleteItem(item)"
+        <v-rating
+          :value="4.5"
+          color="amber"
+          dense
+          half-increments
+          readonly
+          size="14"
+        ></v-rating>
+
+        <div class="grey--text ml-4">
+          4.5 (413)
+        </div>
+      </v-row>
+
+      <div class="my-4 subtitle-1">
+        € • 10 - 25
+      </div>
+
+      <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+    </v-card-text>
+
+    <v-divider class="mx-4"></v-divider>
+
+    <v-card-title>Catégories</v-card-title>
+
+    <v-card-text>
+      <v-chip-group
+        v-model="selection"
+        active-class="amber darken-3 white--text"
+        column
       >
-        mdi-delete
-      </v-icon>
-    </template>
-    <template v-slot:no-data>
+        <v-chip>Homme</v-chip>
+
+        <v-chip>Femme</v-chip>
+
+        <v-chip>Enfants</v-chip>
+
+      </v-chip-group>
+    </v-card-text>
+
+    <v-card-actions>
       <v-btn
-        color="primary"
-        @click="initialize"
+        color="amber darken-2"
+        text
+        @click="reserve"
       >
-        Reset
+        Découvrez nos offres
       </v-btn>
+    </v-card-actions>
+  </v-card>
+  </v-col>
+
+  <!-- category 2 -------------------------------------------------------------------------------------------------------->
+  <!-- category 2 -------------------------------------------------------------------------------------------------------->
+  <v-col>
+  <v-card
+    :loading="loading"
+    class="mx-auto my-6"
+    max-width="374"
+  >
+    <template slot="progress">
+      <v-progress-linear
+        color="deep-purple"
+        height="10"
+        indeterminate
+      ></v-progress-linear>
     </template>
-</v-data-table>
+
+    <v-img
+      height="250"
+      src="https://i.imgur.com/1NNGyBy.png"
+    ></v-img>
+
+    <v-card-title>SÉLECTION BON PLAN</v-card-title>
+
+    <v-card-text>
+      <v-row
+        align="center"
+        class="mx-0"
+      >
+        <v-rating
+          :value="4.5"
+          color="amber"
+          dense
+          half-increments
+          readonly
+          size="14"
+        ></v-rating>
+
+        <div class="grey--text ml-4">
+          4.5 (413)
+        </div>
+      </v-row>
+
+      <div class="my-4 subtitle-1">
+        € • 10 - 25
+      </div>
+
+      <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+    </v-card-text>
+
+    <v-divider class="mx-4"></v-divider>
+
+    <v-card-title>Catégories</v-card-title>
+
+    <v-card-text>
+      <v-chip-group
+        v-model="selection"
+        active-class="amber darken-3 white--text"
+        column
+      >
+        <v-chip>Homme</v-chip>
+
+        <v-chip>Femme</v-chip>
+
+        <v-chip>Enfants</v-chip>
+
+      </v-chip-group>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        color="amber darken-2"
+        text
+        @click="reserve"
+      >
+        Découvrez nos offres
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+  </v-col>
+  
+  <!-- category 3 -------------------------------------------------------------------------------------------------------->
+  <!-- category 3 -------------------------------------------------------------------------------------------------------->
+
+  <v-col>
+  <v-card
+    :loading="loading"
+    class="mx-auto my-6"
+    max-width="374"
+  >
+    <template slot="progress">
+      <v-progress-linear
+        color="deep-purple"
+        height="10"
+        indeterminate
+      ></v-progress-linear>
+    </template>
+
+    <v-img
+      height="250"
+      src="https://i.imgur.com/1NNGyBy.png"
+    ></v-img>
+
+    <v-card-title>SÉLECTION BON PLAN</v-card-title>
+
+    <v-card-text>
+      <v-row
+        align="center"
+        class="mx-0"
+      >
+        <v-rating
+          :value="4.5"
+          color="amber"
+          dense
+          half-increments
+          readonly
+          size="14"
+        ></v-rating>
+
+        <div class="grey--text ml-4">
+          4.5 (413)
+        </div>
+      </v-row>
+
+      <div class="my-4 subtitle-1">
+        € • 10 - 25
+      </div>
+
+      <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+    </v-card-text>
+
+    <v-divider class="mx-4"></v-divider>
+
+    <v-card-title>Catégories</v-card-title>
+
+    <v-card-text>
+      <v-chip-group
+        v-model="selection"
+        active-class="amber darken-3 white--text"
+        column
+      >
+        <v-chip>Homme</v-chip>
+
+        <v-chip>Femme</v-chip>
+
+        <v-chip>Enfants</v-chip>
+
+      </v-chip-group>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        color="amber darken-2"
+        text
+        @click="reserve"
+      >
+        Découvrez nos offres
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+  </v-col>
+</v-row>
 </template>
 <script>
-import { db } from "../firebase";
-export default {
-  name: "ProductList",
-  data() {
-    return {
-      dialog: false,
-      dialogDelete: false,
-      headers: [
-          {
-            text: 'Nom',
-            align: 'start',
-            value: 'name',
-            sortable: false
-          },
-          { text: 'Prix', value: 'price' },
-          { text: 'Actions', value: 'actions', sortable: false },
-        ],
-        products: [],
-        editedIndex: -1,
-        editedItem: {
-            name: '',
-            price: 0,
-        },
-        defaultItem: {
-            name: '',
-            price: 0,
-        },
-    };
-  },
+  export default {
+    data: () => ({
+      loading: false,
+      selection: 0,
+    }),
 
-  computed: {
-      formTitle () {
-        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+    methods: {
+      reserve () {
+        this.loading = true
+
+        setTimeout(() => (this.loading = false), 2000)
       },
     },
-
-    watch: {
-      dialog (val) {
-        val || this.close()
-      },
-      dialogDelete (val) {
-        val || this.closeDelete()
-      },
-    },
-
-
-  methods: {
-      close () {
-        this.dialog = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
-      },
-      
-  },
-  created() {
-    db.collection("products")
-      .get()
-      .then((querySnapshot) => {
-        //this.products = querySnapshot;
-        querySnapshot.forEach((doc) => {
-          this.products.push(doc.data());
-          //console.log(doc.id, " => ", doc.data());
-        });
-      })
-      .catch((error) => {
-        console.log("Error getting documents: ", error);
-      });
-  },
-};
+  }
 </script>
-<style scoped>
-</style>
--->
