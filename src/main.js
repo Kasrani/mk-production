34f0ff0cdf6da0cee5f7aed13bd8859e9b0dea16
Kasrani <kasrani.mourad@gmail.com>
2021-05-08 03:30:43 +0200

    manage products done :tada:

--- src/main.js
+++ src/main.js
@@ -1,36 +1,3 @@
-/*
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store/store'
-import vuetify from './plugins/vuetify';
-import router from './router/router';
-import firebase from "firebase";
-
-//vuetify.config.productionTip = false
-
-let app
-const initialize = () => {
-  if(!app) {
-    app = new Vue({
-      router,
-      store,
-      vuetify,
-      render: h => h(App),
-    }).$mount('#app')
-  }
-}
-
-firebase.auth().onAuthStateChanged(user => {
-  if(user) {
-    store.commit('setCurrentUser', user)
-  } else {
-    store.commit('setCurrentUser', null)
-  }
-
-  initialize()
-})
-*/
-
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -39,6 +6,23 @@ import vuetify from './plugins/vuetify';
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
+import VueFirestore from 'vue-firestore'
+require('firebase/firestore')
+
+Vue.use(VueFirestore, {
+    key: 'id',         // the name of the property. Default is '.key'.
+    enumerable: true  //  whether it is enumerable or not. Default is true.
+})
+
+
+
+
+Vue.use(VueFirestore)
+
+
+import Swal from 'sweetalert2';
+
+window.Swal = Swal;
 
 Vue.config.productionTip = false
 
